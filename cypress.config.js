74e4d4c7e300d09db2afbd7a6137716908efe73d
Papaya Shark <60260401+papayashark@users.noreply.github.com>
2025-06-24 19:48:31 +0200

    fix: Updated cypress.config.js template

--- cypress.config.js
+++ cypress.config.js
@@ -1,36 +1,34 @@
-const { defineConfig } = require("cypress");
-const cypressGrep = require("@cypress/grep");
+import { defineConfig } from 'cypress';
+import cypressGrep from '@cypress/grep';
+import mochawesome from 'cypress-mochawesome-reporter/plugin.js';
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      cypressGrep(config); // plugin only
-      require("cypress-mochawesome-reporter/plugin")(on);
+      cypressGrep(config); // Enables @cypress/grep
+      mochawesome(on);     // Enables mochawesome reporter
       return config;
     },
-    supportFile: "cypress/support/e2e.js", // or index.js if you're using older versions
+    supportFile: 'cypress/support/e2e.js', // Optional if using default
   },
-
   env: {
     appUrl: 'https://magento.softwaretestingboard.com/customer/account/login',
-    grepFilterSpecs: true,
+    grepFilterSpecs: true
   },
-
   retries: 1,
   viewportHeight: 900,
   viewportWidth: 1440,
   video: true,
   videoCompression: true,
-
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: "cypress/reports",
+    reportDir: 'cypress/reports',
     charts: true,
     reportPageTitle: 'luma e-commerce report',
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
     overwrite: false,
-    html: true,
+    html: true
   }
 });
